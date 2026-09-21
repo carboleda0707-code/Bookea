2d61049ec47beb2e81e66c916d22b9f020bb25c7
Carlos Enrique Arboleda Bernita <carboleda0707@gmail.com>
2026-09-20 19:31:51 -0500

    Actualizar API_URL con variable de entorno en frontend.py

--- app/frontend/frontend.py
+++ app/frontend/frontend.py
@@ -42,7 +42,8 @@ except ImportError:
 
 configurar_puente_html()
 
-API_URL = "http://127.0.0.1:8000"
+
+API_URL = os.getenv("API_URL", "http://127.0.0.1:8000")
 
 st.set_page_config(page_title="Bookea - Sistema de Reservas", layout="wide")
 
