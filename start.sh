9898d98299007736755499a97531d5513611bb86
Carlos Enrique Arboleda Bernita <carboleda0707@gmail.com>
2026-09-21 23:57:36 -0500

    Optimizar parámetros de inicio en start.sh para Railway

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 uvicorn app.main:app --host 0.0.0.0 --port 8000 &
-exec streamlit run app/frontend/frontend.py --server.port $PORT --server.address 0.0.0.0
+exec streamlit run app/frontend/frontend.py --server.port $PORT --server.address 0.0.0.0 --server.headless true --server.enableCORS false --server.enableXsrfProtection false
