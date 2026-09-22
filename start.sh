64d2c8dc7004ca713dac7d32a1bc1384cf43a8d2
Carlos Enrique Arboleda Bernita <carboleda0707@gmail.com>
2026-09-22 14:58:56 -0500

    Asegurar ejecución persistente con python -m streamlit

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 python -m uvicorn app.main:app --host 0.0.0.0 --port 8000 &
-streamlit run app/frontend/frontend.py --server.port $PORT --server.address 0.0.0.0 --server.headless true
+exec python -m streamlit run app/frontend/frontend.py --server.port $PORT --server.address 0.0.0.0 --server.headless true
