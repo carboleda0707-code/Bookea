177dd087f9049b703da943518c4eb97bef093de4
Carlos Enrique Arboleda Bernita <carboleda0707@gmail.com>
2026-09-22 22:30:24 -0500

    Configurar start.sh para levantar FastAPI y Streamlit

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-# 1. Levantar FastAPI en segundo plano en el puerto 8000 (usando uvicorn)
-uvicorn app.main:app --host 0.0.0.0 --port 8000 &
+# 1. Levantar FastAPI en segundo plano obligatorios en el puerto 8000
+uvicorn app.main:app --host 127.0.0.1 --port 8000 &
 
-# 2. Levantar Streamlit en el puerto asignado por Railway ($PORT)
+# 2. Levantar Streamlit usando el puerto de Railway
 exec streamlit run app/frontend/frontend.py --server.port $PORT --server.address 0.0.0.0 --server.headless true --server.enableCORS false
