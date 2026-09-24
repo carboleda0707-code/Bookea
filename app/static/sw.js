acbc90e548a7fb5229f353f11ac013970d24ba1e
Carlos Enrique Arboleda Bernita <carboleda0707@gmail.com>
2026-09-24 16:23:18 -0500

    Actualizar Service Worker para PWA

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,4 +1,13 @@
-self.addEventListener('fetch', function(event) {
-  // Service worker básico para habilitar estándares de PWA
-  return;
+// sw.js - Service Worker básico para PWA
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Permite que todas las peticiones fluyan con normalidad
+  event.respondWith(fetch(event.request));
 });
